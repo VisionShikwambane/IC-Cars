aa36d87ad3e5f03f0bfa20ed69c72d1bdb5b9f7b
VisionShikwambane <visionvee201@gmail.com>
2025-02-09 21:01:32 +0200

    changes

--- APP/src/app/user/user-navbar/user-navbar.component.ts
+++ APP/src/app/user/user-navbar/user-navbar.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './user-navbar.component.html',
   styleUrl: './user-navbar.component.css'
 })
